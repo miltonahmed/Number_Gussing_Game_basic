dd2b9aed5b8a21a6db13f78bbf6f890ee4518371
Milton <miltonahmed08@yahoo.com>
2025-06-24 06:36:28 +0400

    Update script.js
    // Select the Player One submit button
    l

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,5 @@
 // Select the input field for Player One
 let PlayerOneInputField = document.querySelector(".input_text_Box_one");
+// Select the Player One submit button
+let PlayerOneButton = document.querySelector(".player_one_submit_btn");
+
