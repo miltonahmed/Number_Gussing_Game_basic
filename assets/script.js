56ff31e253e6adbf20e4cc019b286b3f432fc36b
Milton <miltonahmed08@yahoo.com>
2025-06-24 06:42:50 +0400

    Update script.js
    // When Player One clicks the button, run a function

--- assets/script.js
+++ assets/script.js
@@ -5,3 +5,7 @@ let PlayerOneButton = document.querySelector(".player_one_submit_btn");
 // Select the warning message element for Player One
 let PlayerOneTextWarning = document.querySelector(".player_one_warning");
 
+// When Player One clicks the button, run a function
+PlayerOneButton.addEventListener("click", function(){
+  
+});
