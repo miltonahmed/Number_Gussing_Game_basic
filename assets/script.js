4438cd2f7b53f48259f9a320ba7770c88cf87045
Milton <miltonahmed08@yahoo.com>
2025-06-24 06:38:27 +0400

    Update script.js
    // Select the warning message element for Player One

--- assets/script.js
+++ assets/script.js
@@ -2,4 +2,6 @@
 let PlayerOneInputField = document.querySelector(".input_text_Box_one");
 // Select the Player One submit button
 let PlayerOneButton = document.querySelector(".player_one_submit_btn");
+// Select the warning message element for Player One
+let PlayerOneTextWarning = document.querySelector(".player_one_warning");
 
